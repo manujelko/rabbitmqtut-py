c595ae073b1378e41f419f9a381355758df25d6a
Manuel Šarić <manujelko@gmail.com>
2022-08-14 12:48:23 +0200

    Refactor send

--- hello_world/send.py
+++ hello_world/send.py
@@ -1,12 +1,17 @@
 import pika
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-channel = connection.channel()
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+    channel = connection.channel()
 
-channel.queue_declare(queue="hello")
+    channel.queue_declare(queue="hello")
 
-channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
-print(" [x] Sent 'Hello World!'")
+    channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+    print(" [x] Sent 'Hello World!'")
 
-connection.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
