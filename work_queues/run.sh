ffebea2565c554b96881d21e4f176ff20afd6b38
Manuel Šarić <manujelko@gmail.com>
2022-08-14 13:14:28 +0200

    Work queues

---
+++ work_queues/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for message in one. two.. three... four.... five..... six......
+do
+    python new_task.py $message
+done
---
+++ work_queues/worker.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import time
+
+import pika
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r", body.decode())
+    time.sleep(body.count(b"."))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+    channel = connection.channel()
+
+    channel.queue_declare("task_queue", durable=True)
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue="task_queue", on_message_callback=callback)
+
+    print(" [*] Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
